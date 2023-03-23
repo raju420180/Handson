62e7a81f8427e3cc3df4ce1d9a707ab807ece544
Raju Gundala <r.gundala@f5.com>
2023-03-23 12:07:55 +0530

    new files

---
+++ inputs.tf
@@ -0,0 +1,30 @@
+variable "region" {
+  type        = string
+  default     = "us-west-2"
+  description = "Region to create resources"
+}
+variable "ntier-vpc-range" {
+type        = string
+  default     = "192.168.0.0/16"
+  description = "VPC Cidr Range"
+}
+
+variable "ntier-app1-cidr" {
+  type    = string
+  default = "192.168.0.0/24"
+}
+
+variable "ntier-app2-cidr" {
+  type    = string
+  default = "192.168.1.0/24"
+}
+
+variable "ntier-db1-cidr" {
+  type    = string
+  default = "192.168.2.0/24"
+}
+
+variable "ntier-db2-cidr" {
+  type    = string
+  default = "192.168.3.0/24"
+}
